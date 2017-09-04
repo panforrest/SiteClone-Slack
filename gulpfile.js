4d242cf64e4f4ab62d0a9f4788759aa126c10355
panforrest <guoqianp@gmail.com>
2017-09-04 13:10:30 -0400

    14:30 get-started.html css minimized

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,26 @@ var gp_uglify = require('gulp-uglify')
 var less = require('gulp-less')
 var path = require('path')
 
+gulp.task('get-started', function(){
+    return gulp.src(
+            [
+                './assets/css/rollup-slack_kit_legacy_adapters.css',
+                './assets/css/rollup-plastic.css',
+                './assets/css/find_team.css',
+                './assets/css/lato-2-compressed.css',
+                './assets/css/sticky_nav.css',
+                './assets/css/footer.css',
+                './assets/css/_helpers.css'
+
+            ]
+        )
+        .pipe(sourcemaps.init())
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gp_concat('get-started.min.css'))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest('./dist/css/'))
+})
 
 gulp.task('css', function(){
     return gulp.src(
@@ -27,7 +47,7 @@ gulp.task('css', function(){
         .pipe(gulp.dest('./dist/css/'))
 })
 
-gulp.task('style', ['css'], function(){})
+gulp.task('style', ['css', 'get-started'], function(){})
 
 
 gulp.task('js', function(){
