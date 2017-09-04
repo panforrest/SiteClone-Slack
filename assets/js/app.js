61094bd906e83b6d748c0357c0a57277efebd6cb
panforrest <guoqianp@gmail.com>
2017-09-04 13:35:02 -0400

    button successfully bind to function

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,11 @@
 
   console.log('Hello Slack Clone!')
 
+  var turbo = Turbo({site_id: '59ad89f7e6317e0011a24131'})
+
+  $('#btn-sign-up').click(function(event){
+  	event.preventDefault()
+  	console.log('SIGN UP!')
+  })
+
 })()
