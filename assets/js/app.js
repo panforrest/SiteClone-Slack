e35377d343fcc9738189bb5456f61a2342ed3419
panforrest <guoqianp@gmail.com>
2017-09-04 13:51:41 -0400

    use jquery turbo sdk to post users to backend

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,30 @@
 
   $('#btn-sign-up').click(function(event){
   	event.preventDefault()
-  	console.log('SIGN UP!')
+
+  	var email = $('#input-sign-up').val()
+    if (email == null){
+    	alert('Please Enter Your Email Address')
+    	return
+    }
+
+    if (email.length == 0){
+        alert('Please Enter Your Email Address')
+        return
+    }
+
+  	console.log('SIGN UP!'+email)
+  	
+
+  	turbo.createUser({email: email}, function(err, data){
+  	  if (err){
+  	  	alert('Error: '+err.message)
+  	  	return
+  	  }
+
+  	  console.log('USER REGISTERED: '+JSON.stringify(data))
+  	  window.location.href = '/get-started'
+  	})
   })
 
 })()
